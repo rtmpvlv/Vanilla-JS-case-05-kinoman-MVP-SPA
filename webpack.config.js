cb1fb3cd192335af8c96e070dbd27024fa9de0c0
Artem Pavlov <rtmpvlv@gmail.com>
2021-10-24 18:10:33 +0300

    Installed required node modules

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    watchContentBase: true,
+  }
+};
